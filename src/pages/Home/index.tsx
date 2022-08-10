f7525e2c308b5e1c735a3cfc6ca401bca8c85ae2
彭家乐 <pengjiale@izaiqi.com>
2022-08-11 01:19:02 +0800

    feat: 修改布局

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,21 @@
 import DocList from '@/pages/Home/components/DocList';
+import NavList from '@/pages/Home/components/NavList';
+import TagList from '@/pages/Home/components/TagList';
 import { Col, Row } from 'antd';
 import React from 'react';
 import styles from './index.less';
 
 const Home: React.FC = () => {
   return (
-    <Row className={styles.row} justify={'center'}>
+    <Row className={styles.row}>
+      <Col span={6} className={styles.leftCol}>
+        <TagList />
+      </Col>
       <Col span={12} className={styles.col}>
         <DocList />
+      </Col>
+      <Col span={6} className={styles.leftCol}>
+        <NavList />
       </Col>
     </Row>
   );
